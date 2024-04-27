e66d92cbabab96472f6e5ebfa223a122618040a3
Alan Carlos <alanclash2541@gmail.com>
2024-04-27 20:30:34 -0300

    feat: Implement Login in Angular Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -12,7 +13,8 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { LoginResponse } from '../login/login-response.payload';
+import { LoginRequestPayload } from './../login/login-request.payload';
 import { SignupRequestPayload } from './../signup/signup-request.payload';
 
 @Injectable({
@@ -15,5 +18,19 @@ export class AuthService {
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.httpClient.post(`${this.BASE_URL}/signup`, signupRequestPayload, { responseType: 'text' });
+  }
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<Boolean> {
+    return this.httpClient
+      .post<LoginResponse>(`${this.BASE_URL}/login`, loginRequestPayload)
+      .pipe(map(data => {
+
+        localStorage.setItem('authenticationToken', data.authenticationToken);
+        localStorage.setItem('refreshToken', data.refreshToken);
+        localStorage.setItem('expiresAt', data.expiresAt.toString());
+        localStorage.setItem('username', data.username);
+
+        return true;
+      }));
   }
 }
